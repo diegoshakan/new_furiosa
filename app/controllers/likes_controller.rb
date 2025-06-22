1b69229af5802794495e2817e6f415d668645b66
Diego Araujo <diego_shakan@hotmail.com>
2025-06-22 12:08:16 -0300

    curtida dinâmica.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,17 +3,17 @@ class LikesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    announcement = Announcement.find(params[:announcement_id])
-    like = announcement.likes.find_by(user: current_user)
+    @announcement = Announcement.find(params[:announcement_id])
+    like = @announcement.likes.find_by(user: current_user)
 
     if like
       like.destroy # Remove a curtida se já existir
     else
-      announcement.likes.create(user: current_user) # Adiciona a curtida
+      @announcement.likes.create(user: current_user) # Adiciona a curtida
     end
 
     respond_to do |format|
-      format.html { redirect_back fallback_location: announcement_path(announcement) }
+      format.html { redirect_back fallback_location: announcement_path(@announcement) }
       format.turbo_stream
     end
   end
