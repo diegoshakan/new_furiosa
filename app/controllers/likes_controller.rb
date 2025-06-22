ed81f8c176238a9ccb588b9c0f943fe52242cbb5
Diego Araujo <diego_shakan@hotmail.com>
2025-06-22 11:30:51 -0300

    botão muda se o usuário já tiver curtido

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,13 +3,18 @@ class LikesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @announcement = Announcement.find(params[:announcement_id])
-    @like = @announcement.likes.build(user: current_user)
-    if @like.save
-      respond_to do |format|
-        format.turbo_stream
-        format.html { redirect_to @announcement }
-      end
+    announcement = Announcement.find(params[:announcement_id])
+    like = announcement.likes.find_by(user: current_user)
+
+    if like
+      like.destroy # Remove a curtida se já existir
+    else
+      announcement.likes.create(user: current_user) # Adiciona a curtida
+    end
+
+    respond_to do |format|
+      format.html { redirect_back fallback_location: announcement_path(announcement) }
+      format.turbo_stream
     end
   end
 end
