980e0f1668d854efc9b5148ef667fe66642de98d
Diego Araujo <diego_shakan@hotmail.com>
2025-06-22 11:28:40 -0300

    retornar para página que estava após logar para curtir ou comentar, add novo comentario nav_bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_action :store_user_location!, if: :storable_location?
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def after_sign_in_path_for(resource)
+    session.delete(:user_return_to) || super
+  end
+
+  private
+
+  # Armazena a URL atual para redirecionar após o login
+  def store_user_location!
+    session[:user_return_to] = request.fullpath
+  end
+
+  # Define se a URL deve ser armazenada
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
 end
